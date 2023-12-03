086365334ff144eb4285f314c33d8c5738e02031
Agustin Alberdi <agustinalberdi1@gmail.com>
2023-12-04 00:49:47 +0100

    Add wobble animation directive

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { WobbleDirective } from '../../directives/animations/wobble.directive';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, WobbleDirective],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
