fa2a1b4d1998dbcf092fa0dcc66c2ceef9545a85
Agustin Alberdi <agustinalberdi1@gmail.com>
2023-12-03 20:05:30 +0100

    Fix toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
